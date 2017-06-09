cefb47af7170dd14af7a6fdbca35aa228fed0a31
adamalloul <adampackets@gmail.com>
2017-06-09 20:13:10 +0200

    Added French + cleanup

---
+++ app/src/main/java/com/app/shippy/android/CameraOrGalleryDialog.java
@@ -0,0 +1,53 @@
+package com.app.shippy.android;
+
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+
+/**
+ * Created by adamalloul on 25/12/2016.
+ */
+
+public class CameraOrGalleryDialog extends DialogFragment {
+
+    CameraOrGalleryInterface mListener;
+
+    @Override
+    @NonNull
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        builder.setTitle(getString(R.string.add_profile_picture))
+                .setItems(R.array.camera_or_gallery, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        mListener.onCameraOrGallerySelected(dialog, which);
+                    }
+                });
+        // Create the AlertDialog object and return it
+        return builder.create();
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof CameraOrGalleryInterface) {
+            mListener = (CameraOrGalleryInterface) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement RegistrationPageListener");
+        }
+
+
+    }
+
+
+    interface CameraOrGalleryInterface {
+        void onCameraOrGallerySelected(DialogInterface dialog, int which);
+    }
+}
---
+++ app/src/main/java/com/app/shippy/android/VolleySingleton.java
@@ -0,0 +1,28 @@
+package com.app.shippy.android;
+
+import android.content.Context;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by adamalloul on 14/04/2017.
+ * having a private Context mCtx apparently leads to a memory leak hence the commented parts
+ */
+
+class VolleySingleton {
+
+    private static VolleySingleton mInstance;
+    private RequestQueue mRequestQueue;
+
+    private VolleySingleton(Context context) {
+        mRequestQueue = Volley.newRequestQueue(context.getApplicationContext());
+    }
+
+    static synchronized VolleySingleton getInstance(Context context) {
+        if (mInstance == null) {mInstance = new VolleySingleton(context);}
+        return mInstance;
+    }
+
+    RequestQueue getRequestQueue() {return mRequestQueue;}
+
+}
